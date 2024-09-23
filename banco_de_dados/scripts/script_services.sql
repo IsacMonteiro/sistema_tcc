b03aaca6104faf94a8cc7ba96d3124e7632b732c
Isac Monteiro de Castro Soares <102876877+IsacMonteiro@users.noreply.github.com>
2024-09-23 12:00:58 -0300

    Update script_services.sql

--- banco_de_dados/scripts/script_services.sql
+++ banco_de_dados/scripts/script_services.sql
@@ -1,5 +1,5 @@
--- Coorientador padrão com id 0 para representar "Sem coorientador"
+-- Coorientador padrão com id 1 para representar "Sem coorientador"
 INSERT INTO `sistema_tcc`.`Coorientador` 
-  (`id_coorientador`, `matricula_coorientador`, `nome_coorientador`, `titulacao`, `area_atuacao`, `email_coorientador`) 
+  (`matricula_coorientador`, `nome_coorientador`, `titulacao`, `area_atuacao`, `email_coorientador`) 
 VALUES 
-  (0, '00000', 'Sem coorientador', 'N/A', 'N/A', 'semcoorientador@example.com');
+  ('00000', 'Sem coorientador', 'N/A', 'N/A', 'semcoorientador@example.com');
