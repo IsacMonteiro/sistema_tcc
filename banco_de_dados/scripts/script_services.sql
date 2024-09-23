8b0d7cb21f58e489f56e376306ee8e05259360c9
Isac Monteiro <isacmonteiro398@gmail.com>
2024-09-23 11:58:44 -0300

    Add via VSCode

---
+++ banco_de_dados/scripts/script_services.sql
@@ -0,0 +1,5 @@
+-- Coorientador padrão com id 0 para representar "Sem coorientador"
+INSERT INTO `sistema_tcc`.`Coorientador` 
+  (`id_coorientador`, `matricula_coorientador`, `nome_coorientador`, `titulacao`, `area_atuacao`, `email_coorientador`) 
+VALUES 
+  (0, '00000', 'Sem coorientador', 'N/A', 'N/A', 'semcoorientador@example.com');
