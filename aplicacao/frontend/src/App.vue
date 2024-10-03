8805b7b325317d525ede89619ae34c4bc144e329
Isac Monteiro <isacmonteiro398@gmail.com>
2024-10-03 17:40:29 -0300

    Update via VSCode

---
+++ aplicacao/frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <FormularioCadastro/>
+  </div>
+</template>
+
+<script>
+import FormularioCadastro from './components/CadastroForm.vue';
+
+export default {
+  components: {
+    FormularioCadastro
+  }
+};
+</script>
+
+<style>
+/* Estilos globais aqui */
+</style>
---
+++ aplicacao/frontend/src/components/CadastroForm.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="cadastro-obra">
+      <h1>Cadastro de Obra</h1>
+      <form @submit.prevent="submitForm">
+        
+        <!-- Campos do Curso -->
+        <h2>Dados do Curso</h2>
+        <div>
+          <label for="nome_curso">Nome do Curso:</label>
+          <input type="text" id="nome_curso" v-model="curso.nome_curso" required />
+        </div>
+  
+        <!-- Botão de Envio -->
+        <button type="submit">Cadastrar Obra</button>
+        
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        curso: {
+          nome_curso: ''
+        }
+      };
+    },
+    methods: {
+      submitForm() {
+        axios.post('http://localhost:3000/api/curso', this.curso)
+          .then(response => {
+            console.log('Curso cadastrado:', response.data);
+          })
+          .catch(error => {
+            console.error('Erro ao cadastrar curso:', error);
+          });
+      }
+    }
+  };
+  </script>
+  
+  <style src="../css/FormularioCadastro.css"></style>  
